a6a8828e1d624642f63686d65b2e2877ab9343d3
shaker <youssefshaker2cool@gmail.com>
2025-01-18 14:38:46 +0200

    finch employee page check

--- app/Filament/Employee/Resources/CheckResource.php
+++ app/Filament/Employee/Resources/CheckResource.php
@@ -32,8 +32,9 @@ class CheckResource extends Resource
     protected function getTableQuery(): Builder
     {
         return parent::getTableQuery()
-            ->where('employee_id', Auth::guard('employee')->user()->id); // Replace 'status' and 'active' with your condition
+            ->where('employee_id', Auth::guard('employee')->user()->id);
     }
+
 
     public static function form(Form $form): Form
     {
@@ -43,12 +44,16 @@ class CheckResource extends Resource
                 TextInput::make('employee_id')->default(Auth::guard('employee')->user()->id)->disabled(),
                 TextInput::make('name')->default(Auth::guard('employee')->user()->first_name)->disabled(),
                 DatePicker::make('day')->default(now()),
-                TimePicker::make('check_in')->default(now()->addHours(3)),
+                TimePicker::make('check_in')->default(now()->addHours(2)),
                 TimePicker::make('check_out'),
             ]);
     }
 
-
+    //create علشان اشيل زرار ال 
+    public static function canCreate(): bool
+    {
+        return false;
+    }
 
     public static function table(Table $table): Table
     {
@@ -64,146 +69,142 @@ class CheckResource extends Resource
                     ->label('Day'),
 
                 Tables\Columns\TextColumn::make('check_in')
-                    ->label('Check In')
+                    ->label('checkin')
                     ->formatStateUsing(function ($state) {
                         return \Carbon\Carbon::parse($state)->format('H:i');
                     }),
+
                 Tables\Columns\TextColumn::make('check_out')
                     ->label('Check Out')
                     ->formatStateUsing(function ($state) {
                         return \Carbon\Carbon::parse($state)->format('H:i');
                     }),
 
-                    Tables\Columns\TextColumn::make('break_time')
+                Tables\Columns\TextColumn::make('break_time')
                     ->label('Break (Mins)')
                     ->formatStateUsing(function ($state) {
-                        return number_format($state, 2); 
+                        return number_format($state, 2);
                     }),
 
             ])
-            ->defaultSort('check_out', 'desc')
+            ->defaultSort('id', 'desc')
             ->filters([
                 //
             ])
             ->headerActions([
-                        // End Break
+                // End Break
                 Tables\Actions\Action::make('EndBreak')
-                ->label('End Break')
-                ->action(function () {
-                    if (!self::todayCheck()) {
-                        self::sendNotifications('Error!', 'No Breaks Allowed Out Of The Shift!');
+                    ->label('End Break')
+                    ->action(function () {
+                        if (!self::todayCheck()) {
+                            self::sendNotifications('Error!', 'No Breaks Allowed Out Of The Shift!');
+                            return;
+                        }
+                        if (!session()->get('stopwatch')) {
+                            self::sendNotifications('Warning!', 'You Must Start Break Timer First!');
+                            return;
+                        }
+
+                        $start = session()->get('stopwatch');
+                        $start = Carbon::createFromFormat('H:i:s', $start);  // تأكد من تنسيق الوقت بشكل صحيح
+                        $endTime = Carbon::now(); // الحصول على الوقت الحالي
+                        $elapsed = $start->diffInMinutes($endTime); // حساب الفرق في الدقائق
+                        $elapsed = number_format($elapsed, 2);
+
+                        self::sendNotifications('Success!', "Your Break Has Just Ended With {$elapsed} Mins!");
+                        self::logMessage(now() . " => " . Auth::guard('employee')->user()->first_name . " took a break for " . $elapsed . " Mins");
+
+                        // تحديث الجلسة
+                        session()->forget('stopwatch');
+                        $myBreaks = session()->get('breaks', 0); // إذا لم تكن القيمة موجودة في الجلسة، اجعلها صفر
+                        $myBreaks += $elapsed;
+                        session()->put('breaks', $myBreaks);
                         return;
-                    }
-                    if (!session()->get('stopwatch')) {                            
-                        self::sendNotifications('Warning!', 'You Must Start Break Timer First!');
-                        return;
-                    }
-
-                    $start = session()->get('stopwatch');
-                    $start = Carbon::createFromFormat('H:i:s', $start);  // تأكد من تنسيق الوقت بشكل صحيح
-                    $endTime = Carbon::now(); // الحصول على الوقت الحالي
-                    $elapsed = $start->diffInMinutes($endTime); // حساب الفرق في الدقائق
-                    $elapsed = number_format($elapsed, 2);
-
-                    self::sendNotifications('Success!', "Your Break Has Just Ended With {$elapsed} Mins!");
-                    self::logMessage(now() . " => " . Auth::guard('employee')->user()->first_name . " took a break for " . $elapsed . " Mins");
-
-                    // تحديث الجلسة
-                    session()->forget('stopwatch');
-                    $myBreaks = session()->get('breaks', 0); // إذا لم تكن القيمة موجودة في الجلسة، اجعلها صفر
-                    $myBreaks += $elapsed;
-                    session()->put('breaks', $myBreaks);
-                    return;
-                })
-                ->color('warning')
-                ->visible(function () {
-                    return session()->get('stopwatch') !== null;
-                }),
+                    })
+                    ->color('warning')
+                    ->visible(function () {
+                        return session()->get('stopwatch') !== null;
+                    }),
 
                 // Start Break
                 Tables\Actions\Action::make('StartBreak')
-                ->label('Start Break')
-                ->action(function () {
-                    if (!session()->has('breaks')) {
-                        session()->put('breaks', 0);
-                    }
-                    if (!self::todayCheck()) {
-                        self::sendNotifications('Error!', 'No Breaks Allowed Out Of The Shift!');
-                        return;
-                    }
-                    if (!session()->get('stopwatch')) {                            
-                        $currentTime = Carbon::now();
-                        session()->put('stopwatch', $currentTime->format('H:i:s'));  // تخزين الوقت في الجلسة
-                        self::sendNotifications('Success!', 'Your Break Has Just Started!');
-                        return;
-                    }
-                    self::sendNotifications('Error!', 'Your Break Has Already Started');
-                })
-                ->color('warning')
-                ->visible(function () {
-                    return session()->get('check_out') === false && session()->get('stopwatch') === null;
-                }),
+                    ->label('Start Break')
+                    ->action(function () {
+                        if (!session()->has('breaks')) {//has result true or false
+                            session()->put('breaks', 0);//put value to session
+                        }
+                        if (!self::todayCheck()) {
+                            self::sendNotifications('Error!', 'No Breaks Allowed Out Of The Shift!');
+                            return;
+                        }
+                        if (!session()->get('stopwatch')) {
+                            $currentTime = Carbon::now();
+                            session()->put('stopwatch', $currentTime->format('H:i:s'));  // تخزين الوقت في الجلسة
+                            self::sendNotifications('Success!', 'Your Break Has Just Started!');
+                            return;
+                        }
+                        self::sendNotifications('Error!', 'Your Break Has Already Started');
+                    })
+                    ->color('warning')
+                    ->visible(function () {
+                        return session()->get('check_out') === false && session()->get('stopwatch') === null;
+                    }),
 
                 // Check-out
                 Tables\Actions\Action::make('CheckOut')
-                ->label('Check-out')
-                ->action(function () {
-                    $todayCheck = self::todayCheck();
-                    if (!$todayCheck) {
-                        self::sendNotifications('Warning!', 'You Must Check-in First!');
-                        return;
-                    }
-                    session()->put('check_out', true);
-                    if (!session()->get('stopwatch')) {
-                        $todayCheck->update([
-                            'check_out' => now()->addHours(3),
-                            'break_time' => session()->get('breaks') > 0 ? session()->get('breaks') : 0
-                        ]);
-                        session()->forget('breaks');
-                        self::logMessage(now() . " => " . Auth::guard('employee')->user()->first_name . " finished working.");
-                        return;
-                    }
-                    self::sendNotifications('Warning!', 'You Must Stop Break First!');
-                })
-                ->color('danger')
-                ->visible(function () {
-                    return self::todayCheck() !== null && !session()->get('check_out') && session()->get('stopwatch') === null;
-                }),
+                    ->label('Check-out')
+                    ->action(function () {
+                        $todayCheck = self::todayCheck();
+                        if (!$todayCheck) {
+                            self::sendNotifications('Warning!', 'You Must Check-in First!');
+                            return;
+                        }
+                        session()->put('check_out', true);
+                        if (!session()->get('stopwatch')) {
+                            $todayCheck->update([
+                                'check_out' => now()->addHours(2),
+                                'break_time' => session()->get('breaks') > 0 ? session()->get('breaks') : 0
+                            ]);
+                            session()->forget('breaks');
+                            self::logMessage(now() . " => " . Auth::guard('employee')->user()->first_name . " finished working.");
+                            return;
+                        }
+                        self::sendNotifications('Warning!', 'You Must Stop Break First!');
+                    })
+                    ->color('danger')
+                    ->visible(function () {
+                        return self::todayCheck() !== null && !session()->get('check_out') && session()->get('stopwatch') === null;
+                    }),
 
                 // Check-in
                 Tables\Actions\Action::make('CheckIn')
-                ->label('Check-in')
-                ->action(function () {
-                    if (self::todayCheck()) {
-                        self::sendNotifications('Error!', 'You Must Check-out First!');
-                        return;
-                    } else {
-                        Check::create([
-                            'name' => Auth::guard('employee')->user()->first_name,
-                            'employee_id' => Auth::guard('employee')->user()->id,
-                            'day' => now(),
-                            'check_in' => now()->addHours(3),
-                        ]);
-                        session()->put('check_out', false);
-                        $logMessage = now() . " => " . Auth::guard('employee')->user()->first_name . " started working.";
-                        self::logMessage($logMessage);
-                        return;
-                    }
-                })
-                ->color('primary')
-                ->visible(function () {
-                    return self::todayCheck() === null;
-                }),
+                    ->label('Check-in')
+                    ->action(function () {
+                        if (self::todayCheck()) {
+                            self::sendNotifications('Error!', 'You Must Check-out First!');
+                            return;
+                        } else {
+                            Check::create([
+                                'name' => Auth::guard('employee')->user()->first_name,
+                                'employee_id' => Auth::guard('employee')->user()->id,
+                                'day' => now(),
+                                'check_in' => now()->addHours(2),
+                            ]);
+                            session()->put('check_out', false);
+                            $logMessage = now() . " => " . Auth::guard('employee')->user()->first_name . " started working.";
+                            self::logMessage($logMessage);
+                            return;
+                        }
+                    })
+                    ->color('primary')
+                    ->visible(function () {
+                        return self::todayCheck() === null;
+                    }),
 
             ])
             ->actions([
                 // Tables\Actions\EditAction::make(),
             ]);
-            // ->bulkActions([
-            //     Tables\Actions\BulkActionGroup::make([
-            //         Tables\Actions\DeleteBulkAction::make(),
-            //     ]),
-            // ]);
     }
 
     public static function getRelations(): array
@@ -213,6 +214,7 @@ class CheckResource extends Resource
         ];
     }
 
+    //بيتاكد ان الموظف سجل الدخول النهاردة و ليا مسجلش الخروج
     public static function todayCheck()
     {
         return Check::where('employee_id', Auth::guard('employee')->user()->id)->where('check_out', null)->first();
@@ -227,7 +229,7 @@ class CheckResource extends Resource
         ];
     }
 
-        public static function sendNotifications($title, $body)
+    public static function sendNotifications($title, $body)
     {
         return Notification::make()
             ->title($title)
@@ -242,5 +244,5 @@ class CheckResource extends Resource
             'message' => $message,
             'employee_id' => Auth::guard('employee')->user()->id
         ]);
-    } 
+    }
 }
--- app/Filament/Employee/Resources/CheckResource/Pages/ListChecks.php
+++ app/Filament/Employee/Resources/CheckResource/Pages/ListChecks.php
@@ -2,9 +2,12 @@
 
 namespace App\Filament\Employee\Resources\CheckResource\Pages;
 
-use App\Filament\Employee\Resources\CheckResource;
+use App\Models\Check;
 use Filament\Actions;
+use Illuminate\Support\Facades\Auth;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Employee\Resources\CheckResource;
 
 class ListChecks extends ListRecords
 {
@@ -15,5 +18,10 @@ class ListChecks extends ListRecords
         return [
             Actions\CreateAction::make(),
         ];
+    }
+
+    protected function getTableQuery(): Builder
+    {
+        return Check::where('employee_id', Auth::guard('employee')->user()->id);
     }
 }
